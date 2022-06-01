bbf829da5fcbbdbb797f4a0c732ffccc61d4206b
xcomagent95 <alex.pilz95@googlemail.com>
2022-06-01 14:32:41 +0200

    Kommentare

--- api.py
+++ api.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+#imports
 from flask import Flask , render_template, jsonify, request, send_file
 import json
 import os
@@ -7,117 +9,117 @@ import utils
 import traceback
 import logging
 
-logging.basicConfig(filename = 'apiLog.log', 
-                    level=logging.INFO, 
-                    format = f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+#set logging options
+logging.basicConfig(filename = 'apiLog.log', #set logfile
+                    level=logging.INFO, #set loglevel
+                    format = f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s') #set logging format
 
 #initialize app
-app = Flask(__name__) #define flask app
+app = Flask(__name__) #initialize application
 
 #landingpage endpoint
 @app.route('/',  methods = ['GET']) #allowed methods: GET
 def getLandingPage():
-    app.logger.info('/')
+    app.logger.info('/') #add log entry when endpoint is called
     try:
-        if(request.content_type == "text/html" or 
-           request.args.get('f')=="text/html"): #check requested content-type
+        if(request.content_type == "text/html" or #check requested content-type from request body
+           request.args.get('f')=="text/html"): #check requested content-type from inline request
             response = render_template('html/landingPage.html') #render static landing page
-            return response, 200, {"link": "localhost:5000/?f=text/html", "resource": "landingPage"} #return response and ok
-        elif(request.content_type == "application/json" or 
-             request.content_type == "application/vnd.oai.openapi+json;version=3.0" or 
-             request.args.get('f')=="application/json"): #check requested content-type
-            file = open('templates/json/landingPage.json',) #open LandingPage.json
-            payload = json.load(file) #create response
-            file.close() #close LandingPage.json
+            return response, 200, {"link": "localhost:5000/?f=text/html", "resource": "landingPage"} #return response and okay with link and resource header
+        elif(request.content_type == "application/json" or #check requested content-type from request body
+             request.args.get('f')=="application/json"): #check requested content-type from inline request
+            file = open('templates/json/landingPage.json',) #open landingPage.json
+            payload = json.load(file) #create payload 
+            file.close() #close landingPage.json
             response = jsonify(payload) #create response
-            response.status_code = 200 #set response code
-            return response, {"link": "localhost:5000/?f=application/json", "resource": "landingPage"} #return response and ok
+            response.status_code = 200 #set response code ok
+            return response, {"link": "localhost:5000/?f=application/json", "resource": "landingPage"} #return response and okay with link and resource header
         else:
-                return "HTTP status code 406: not acceptable", 406 #not acceptable 
+                return "HTTP status code 406: not acceptable", 406 #return not acceptable if requested content-type is not supported
     except:
-        return "HTTP status code 500: internal server error", 500 #internal server error
+        return "HTTP status code 500: internal server error", 500 #retrun internal server error if something went wrong
 
 #conformance endpoint
 @app.route('/conformance',  methods = ['GET']) #allowed methods: GET
 def getConformance():
-    app.logger.info('/conformance')
+    app.logger.info('/conformance') #add log entry when endpoint is called
     try:
-        if(request.content_type == "text/html" or 
-           request.args.get('f')=="text/html"): #check requested content-type
+        if(request.content_type == "text/html" or #check requested content-type from request body
+           request.args.get('f')=="text/html"): #check requested content-type from inline request
             response = render_template('html/confClasses.html') #render static conformance page
-            return response, 200, {"link": "localhost:5000/conformance?f=text/html", "resource": "conformance"} #return response and ok
-        elif(request.content_type == "application/json" or 
-             request.args.get('f')=="application/json"): #check requested content-type
+            return response, 200, {"link": "localhost:5000/conformance?f=text/html", "resource": "conformance"} #return response and okay with link and resource header
+        elif(request.content_type == "application/json" or #check requested content-type from request body
+             request.args.get('f')=="application/json"): #check requested content-type from inline request
             file = open('templates/json/confClasses.json',) #open ConfClasses.json
             payload = json.load(file) #create response
             file.close() #close ConfClasses.json
             response = jsonify(payload) #create response
             response.status_code = 200 #set response code
-            return response, {"link": "localhost:5000/conformance?f=application/json", "resource": "conformance"} #return response and ok
+            return response, {"link": "localhost:5000/conformance?f=application/json", "resource": "conformance"} #return response and okay with link and resource header
         else:
-                return "HTTP status code 406: not acceptable", 406 #not acceptable 
+                return "HTTP status code 406: not acceptable", 406 #return not acceptable if requested content-type is not supported
     except:
-        return "HTTP status code 500: internal server error", 500 #internal server error
+        return "HTTP status code 500: internal server error", 500 #retrun internal server error if something went wrong
     
 #api endpoint
 @app.route('/api',  methods = ['GET']) #allowed methods: GET
 def getAPIDefinition():
-    app.logger.info('/api')
+    app.logger.info('/api') #add log entry when endpoint is called
     try:
-        if(request.content_type == "text/html" or 
-           request.args.get('f')=="text/html"): #check requested content-type
-            response = render_template('html/api/index.html') #render api definition
-            return response, 200, {"link": "localhost:5000/apiDefinition?f=text/html", "resource": "api"}
-        elif(request.content_type == "application/json" or 
-             request.args.get('f')=="application/json"): #check requested content-type
-            file = open('templates/json/apiDefinition.json',) #open ConfClasses.json
+        if(request.content_type == "text/html" or #check requested content-type from request body 
+           request.args.get('f')=="text/html"): #check requested content-type from inline request
+            response = render_template('html/api/index.html') #render static api definition page
+            return response, 200, {"link": "localhost:5000/apiDefinition?f=text/html", "resource": "api"} #return response and okay with link and resource header
+        elif(request.content_type == "application/json" or #check requested content-type from request body 
+             request.args.get('f')=="application/json"): #check requested content-type from inline request
+            file = open('templates/json/apiDefinition.json',) #open apiDefinition.json
             payload = json.load(file) #create response
-            file.close() #close ConfClasses.json
+            file.close() #close apiDefinition.json
             response = jsonify(payload) #create response
             response.status_code = 200 #set response code
-            return response, {"link": "localhost:5000/api?f=application/json", "resource": "api"} #return response and ok
+            return response, {"link": "localhost:5000/api?f=application/json", "resource": "api"} #return response and okay with link and resource header
         else:
-                return "HTTP status code 406: not acceptable", 406 #not acceptable 
+                return "HTTP status code 406: not acceptable", 406 #return not acceptable if requested content-type is not supported
     except Exception:
         traceback.print_exc()
-        return "HTTP status code 500: internal server error", 500 #internal server error
+        return "HTTP status code 500: internal server error", 500 #retrun internal server error if something went wrong
 
 #processes endpoint
 @app.route('/processes', methods = ['GET']) #allowed methods: GET
 def getProcesses():
-    app.logger.info('/processes')
-    if(request.args.get('limit') == None or 
-       int(request.args.get('limit')) <= 0 or 
-       int(request.args.get('limit')) > 1000): #if no limit is set
-        limit = 10 #default value
+    app.logger.info('/processes') #add log entry when endpoint is called
+    if(request.args.get('limit') == None or #if no limit is set
+       int(request.args.get('limit')) <= 0 or #or limit value is not valid
+       int(request.args.get('limit')) > 1000): #or limit is to large
+        limit = 10 #set limit to default value
     else:
-        limit = int(request.args.get('limit')) #get limit from request
+        limit = int(request.args.get('limit')) #retrive limit value from request
     try:
-        if(request.content_type == "text/html" or 
-           request.args.get('f')=="text/html"): #check requested content-type
-                processList = []
-                processDescriptions = os.listdir("templates/json/processes") #list registered processes
-                response = "<!DOCTYPE html><html><body><h1>Processes:</h1>" #initialize HTML document
+        if(request.content_type == "text/html" or #check requested content-type from request body 
+           request.args.get('f')=="text/html"): #check requested content-type from inline request
+                processList = [] #initialize list of processes
+                processDescriptions = os.listdir("templates/json/processes") #list registered process descriptions
                 counter = 0 #initialize counter
-                for i in processDescriptions: #iterate over registered processes
-                    file = open('templates/json/processes/' + i,) #open process descriptions
+                for i in processDescriptions: #iterate over registered process descriptions
+                    file = open('templates/json/processes/' + i,) #open process description
                     process = json.load(file) #load the data from .json file
-                    file.close() #close file
-                    processList.append(process)
-                    if(counter == limit): #check if counter has reached request limit
-                        break 
-                response = render_template('html/processes.html', processes=processList) #render static conformance page
-                return response, 200, {"link": "localhost:5000/processes?f=text/html", "resource": "processes"} #return response and ok
-        elif(request.content_type == "application/json" or 
-             request.args.get('f')=="application/json"): #check requested content-type
-            processDescriptions = os.listdir("templates/json/processes")  #list registered processes
-            processesArray = [] #initialize process array                        
-            for i in processDescriptions: #iterate over registered processes
+                    file.close() #close .json file
+                    processList.append(process) #append process to list of processes
+                    if(counter == limit): #check if counter has reached limit value
+                        break #if limit is reached break loop
+                response = render_template('html/processes.html', processes=processList) #render dynamic process list 
+                return response, 200, {"link": "localhost:5000/processes?f=text/html", "resource": "processes"} #return response and ok with link and resource header
+
+        elif(request.content_type == "application/json" or #check requested content-type from request body 
+             request.args.get('f')=="application/json"): #check requested content-type from inline request
+            processList = [] #initialize list of processes
+            processDescriptions = os.listdir("templates/json/processes")  #list registered process descriptions                 
+            for i in processDescriptions: #iterate over registered process descriptions
                 file = open('templates/json/processes/' + i,) #open process descriptions
                 process = json.load(file) #load the data from .json file
-                file.close() #close file 
-                processesArray.append(process) #append process to processes array
-            processes = {"processes": processesArray[0:limit], #add processes up to limit parameter and links
+                file.close() #close .json file
+                processList.append(process) #append process to list of processes
+            processes = {"processes": processList[0:limit], #add processes up to limit parameter and links
                             "links": [
                                {
                                  "href": "localhost:5000/processes?f=applicattion/json",
@@ -131,18 +133,16 @@ def getProcesses():
                             }
                             ]}
             response = jsonify(processes) #create response
-            response.status_code = 200, {"link": "localhost:5000/processes?f=application/json", "resource": "processes"} #set response code
-            return response #return response and ok
+            return response, 200, {"link": "localhost:5000/processes?f=application/json", "resource": "processes"} #return response and ok with link and resource header
         else:
-            return "HTTP status code 406: not acceptable", 406 #not acceptable 
-    except Exception:
-        traceback.print_exc()
-        return "HTTP status code 500: internal server error", 500 #internal server error
+            return "HTTP status code 406: not acceptable", 406 #return not acceptable if requested content-type is not supported
+    except:
+        return "HTTP status code 500: internal server error", 500 #retrun internal server error if something went wrong
   
 #process endpoint
 @app.route('/processes/<processID>', methods = ['GET']) #allowed methods: GET
 def getProcess(processID):
-    app.logger.info('/processes/' + processID)
+    app.logger.info('/processes/' + processID) #add log entry when endpoint is called
     try:
         if(request.content_type == "text/html" or 
            request.args.get('f')=="text/html"):  #check requested content-type
@@ -174,7 +174,7 @@ def getProcess(processID):
 #execute endpoint
 @app.route('/processes/<processID>/execution', methods = ['POST']) #allowed methods: POST
 def executeProcess(processID):
-    app.logger.info('/processes/' + processID + '/execution')
+    app.logger.info('/processes/' + processID + '/execution') #add log entry when endpoint is called
     try:
         if(os.path.exists('templates/json/processes/' + str(processID) + 'ProcessDescription.json')):  
             data = json.loads(request.data.decode('utf8').replace("'", '"'))
@@ -239,7 +239,7 @@ def executeProcess(processID):
 #jobs endpoint    
 @app.route('/jobs', methods = ['GET'])
 def getJobs():
-    app.logger.info('/jobs')
+    app.logger.info('/jobs') #add log entry when endpoint is called
     if(request.args.get('limit') == None or 
        int(request.args.get('limit')) <= 0 or 
        int(request.args.get('limit')) > 1000):
@@ -369,7 +369,7 @@ def getJobs():
 #job endpoint for status and dismiss
 @app.route('/jobs/<jobID>', methods = ['GET', 'DELETE'])
 def getJob(jobID):
-    app.logger.info('/jobs/' + jobID)
+    app.logger.info('[GET] /jobs/' + jobID) #add log entry when endpoint is called
     if(request.method == 'GET'):
         if(request.content_type == "application/json" or 
            request.args.get('f')=="application/json"):
@@ -404,6 +404,7 @@ def getJob(jobID):
             return "HTTP status code 406: not acceptable", 406 #not acceptable
         
     if(request.method == 'DELETE'):
+        app.logger.info('[DELETE] /jobs/' + jobID) #add log entry when endpoint is called
         try:       
             if(os.path.exists('jobs/' + str(jobID) + '/status.json')):
                 with open('jobs/' + str(jobID) + '/status.json', "r") as f:
@@ -432,7 +433,7 @@ def getJob(jobID):
 
 @app.route('/jobs/<jobID>/results', methods = ["GET"])
 def getResults(jobID):
-    app.logger.info('/jobs/' + jobID + '/results')
+    app.logger.info('/jobs/' + jobID + '/results') #add log entry when endpoint is called
     if(os.path.exists('jobs/' + str(jobID))):
         try:
             file = open('jobs/' + str(jobID) + "/status.json",)
