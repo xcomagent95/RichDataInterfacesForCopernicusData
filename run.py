2ad28b0a02551ae71fb9e646a130da7b1b09fca9
xcomagent95 <alex.pilz95@googlemail.com>
2022-06-01 17:53:28 +0200

    Last Push of the day

--- run.py
+++ run.py
@@ -7,10 +7,10 @@ api = subprocess.Popen(['python', 'api.py']) #start the api in a subprocess
 print("API running...")
 
 #create processing subprocess
-processingChain = subprocess.Popen(['python', 'processing.py']) #start the processing queue in a subprocess
+processing = subprocess.Popen(['python', 'processing.py']) #start the processing queue in a subprocess
 print("Processing running...")
 
 #create test suite subprocess
 
-testSuite = subprocess.Popen(['python', 'testSuit.py']) #start the test suit
+test = subprocess.Popen(['python', 'testSuit.py']) #start the test suit
 print("Test suit running running...")
